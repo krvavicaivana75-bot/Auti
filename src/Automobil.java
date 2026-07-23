8c8d6372272851628182c0d82932cb55c7256aa2
Ivana <krvavica.ivana.75@gmail.com>
2026-07-23 19:07:16 +0200

    Klasa auti

---
+++ src/Automobil.java
@@ -0,0 +1,28 @@
+public class Automobil {
+    private String marka;
+    private String model;
+    private int godinaProizvodnje;
+    private String regOznaka;
+    private int trenutnaBrzina;
+    private boolean upaljen;
+
+    public Automobil(String marka,String model,int godinaProizvodnje,String regOznaka){
+        this.marka=marka;
+        this.model=model;
+        this.godinaProizvodnje=godinaProizvodnje;
+        this.regOznaka=regOznaka;
+        this.trenutnaBrzina=0;
+        this.upaljen=false;
+    }
+    public void ubrzaj(int novaBrzina){
+        this.trenutnaBrzina=novaBrzina;
+
+    }
+    public void uspori(int novaBrzina){
+        this.trenutnaBrzina=novaBrzina;
+
+    }
+
+
+
+}
