d05477964f5b1829b5358ed0030eaec0ff82685d
Ivana <krvavica.ivana.75@gmail.com>
2026-07-23 19:58:09 +0200

    Klasa main izmjena

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String...args){
         Automobil auto1=new Automobil("Zastava","101",1980,"st208zt");
-        auto1.ispisi();
+
         auto1.upali();
-        auto1.ispisi();
+        auto1.setTrenutnaBrzina(10);
+        auto1.ubrzaj(20);
+        auto1.uspori(5);
+        auto1.ugasi();
+
     }
 }
