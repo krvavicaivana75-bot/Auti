77cb8ef151eb37302ec3fa3a1546a42917ba0653
Ivana <krvavica.ivana.75@gmail.com>
2026-07-23 19:50:13 +0200

    Klasa auti izmjena2

--- src/Automobil.java
+++ src/Automobil.java
@@ -14,6 +14,55 @@ public class Automobil {
         this.trenutnaBrzina=0;
         this.upaljen=false;
     }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getGodinaProizvodnje() {
+        return godinaProizvodnje;
+    }
+
+    public void setGodinaProizvodnje(int godinaProizvodnje) {
+        this.godinaProizvodnje = godinaProizvodnje;
+    }
+
+    public String getRegOznaka() {
+        return regOznaka;
+    }
+
+    public void setRegOznaka(String regOznaka) {
+        this.regOznaka = regOznaka;
+    }
+
+    public int getTrenutnaBrzina() {
+        return trenutnaBrzina;
+    }
+
+    public void setTrenutnaBrzina(int trenutnaBrzina) {
+        this.trenutnaBrzina = trenutnaBrzina;
+    }
+
+    public boolean isUpaljen() {
+        return upaljen;
+    }
+
+    public void setUpaljen(boolean upaljen) {
+        this.upaljen = upaljen;
+    }
+
     public void ubrzaj(int novaBrzina){
         this.trenutnaBrzina+=novaBrzina;
 
@@ -23,7 +72,9 @@ public class Automobil {
 
     }
     public void upali(){
+        this.upaljen=true;
         System.out.println("Automobil je upaljen");
+        System.out.println("Upalio si auto, postavi brzinu");
 
     }
     public void ugasi(){
@@ -40,6 +91,18 @@ public class Automobil {
             uspori(10);
         }
         ugasi();
+
+    }
+    public void ispisi(){
+        System.out.println("marka: "+getMarka());
+        System.out.println("model: "+getModel());
+        System.out.println("godina proizvodnje: "+getGodinaProizvodnje());
+        System.out.println("Reg oznaka "+getRegOznaka());
+        if(upaljen){
+            System.out.println("Status "+"upaljen");
+        }else{
+            System.out.println("Status "+"ugašen");
+        }
 
     }
 
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String...args){
+        Automobil auto1=new Automobil("Zastava","101",1980,"st208zt");
+        auto1.ispisi();
+        auto1.upali();
+        auto1.ispisi();
+    }
+}
